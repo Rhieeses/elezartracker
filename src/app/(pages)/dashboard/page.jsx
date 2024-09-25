504d8bd869c4f89f2021eaf62a4bf3c959a8caa8
Rhieeses <168182278+Rhieeses@users.noreply.github.com>
2024-09-25 09:14:36 +0800

    Add files via upload

---
+++ src/app/(pages)/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import DashboardContent from './dashboard';
+
+export const metadata = {
+	title: 'Capstone | Dashboard',
+	description: 'Capstone',
+};
+
+export default function Dashboard() {
+	return <DashboardContent />;
+}
