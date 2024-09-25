e95df92c73fed3d75e40fe7cd82987628ae6f32a
Rhieeses <168182278+Rhieeses@users.noreply.github.com>
2024-09-25 09:13:42 +0800

    Add files via upload

---
+++ src/app/(pages)/accounts/page.jsx
@@ -0,0 +1,10 @@
+import AccountsContent from './accounts';
+
+export const metadata = {
+	title: 'Capstone | Accounts',
+	description: 'Capstone',
+};
+
+export default function Accounts() {
+	return <AccountsContent />;
+}
